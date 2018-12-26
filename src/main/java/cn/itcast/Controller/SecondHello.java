e4743c218e9bdf5528ecc30f2f9b1b34645bc1cc
zengpengkindle <463478044@qq.com>
2018-12-26 12:40:01 +0800

    MyFirstSpringBoot

--- src/main/java/cn/itcast/Controller/SecondHello.java
+++ src/main/java/cn/itcast/Controller/SecondHello.java
@@ -1,7 +1,16 @@
 package cn.itcast.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by netman on 2018/12/26.
  */
+@RestController
 public class SecondHello {
+    @GetMapping("second")
+    public String second()
+    {
+        return "say second";
+    }
 }
