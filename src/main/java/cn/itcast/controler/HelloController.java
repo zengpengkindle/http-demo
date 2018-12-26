ec22460176375d131c9dd61a268bd0c831b85c91
zengpengkindle <463478044@qq.com>
2018-12-26 11:05:03 +0800

    MyFirstSpringBoot

---
+++ src/main/java/cn/itcast/controler/HelloController.java
@@ -0,0 +1,12 @@
+package cn.itcast.controler;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by netman on 2018/12/26.
+ */
+@RestController
+public class HelloController {
+
+}
