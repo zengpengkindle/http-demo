8c9f931516b546252f566f1e5deb01e2df456b8e
zengpengkindle <463478044@qq.com>
2018-12-28 10:04:44 +0800

    MyFirstSpringBoot

--- src/main/java/cn/itcast/config/MVCconfig.java
+++ src/main/java/cn/itcast/config/MVCconfig.java
@@ -1,11 +1,17 @@
 package cn.itcast.config;
 
 import cn.itcast.intercepter.LoginInterceptor;
+import com.zaxxer.hikari.HikariConfigMXBean;
+import com.zaxxer.hikari.HikariDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -25,13 +31,16 @@ import javax.sql.DataSource;
 @Configuration
 public class MVCconfig implements WebMvcConfigurer {
 
+    @Autowired
+    private DataSource dataSource;
+
     //拦截器方式一
-  //  @Override
+    //  @Override
     public void addInterceptors2(InterceptorRegistry registry) {//注册拦截器
 
         registry.addInterceptor(new HandlerInterceptor() {
             //定义self4j日志主键
-            private Logger logger= LoggerFactory.getLogger(this.getClass());
+            private Logger logger = LoggerFactory.getLogger(this.getClass());
 
             @Override
             public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -52,6 +61,30 @@ public class MVCconfig implements WebMvcConfigurer {
 
 
     }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean() {
+        SqlSessionFactoryBean sqlFactory = new SqlSessionFactoryBean();
+        //  SqlSessionFactory
+        try {
+            sqlFactory.setDataSource(dataSource);
+            //设置mybatis的主配置文件  记住是主配置文件啊
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            Resource mybatisXml = resolver.getResource("classpath:mybatis-config.xml");
+            sqlFactory.setConfigLocation(mybatisXml);
+            //设置mapper.xml文件的路径
+            Resource[] resource = resolver.getResources("classpath*:com/jiuzhm/memo/mybatis/**/*.xml");//{resourceMapXML};
+            sqlFactory.setMapperLocations(resource);
+            //扫描实体类所在包
+            sqlFactory.setTypeAliasesPackage("com.jiuzhm.memo.mybatis.entity");
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+        return sqlFactory;
+    }
+
+
+
     @Bean
     public LoginInterceptor loginInterceptor()
     {
